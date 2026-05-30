bdb5239b10beda5484cd810b8dc0a9f7f35e1077
Creeper19472 <creeper19472@crpteam.club>
2026-05-30 19:17:01 +0800

    chore: bump version

--- src/include/constants.py
+++ src/include/constants.py
@@ -21,7 +21,7 @@ from pathlib import Path
 
 from include.classes.version import Version
 
-CORE_VERSION = Version("0.3.0.260527_alpha")
+CORE_VERSION = Version("0.3.0.260530_alpha")
 PROTOCOL_VERSION = 13
 
 ROOT_ABSPATH = Path(__file__).resolve().parent.parent
