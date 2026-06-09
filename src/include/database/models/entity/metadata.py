4eaf03d237a018c1e595bdcc6434922b44fa14f5
Creeper19472 <creeper19472@crpteam.club>
2026-06-09 12:20:31 +0800

    feat: add relationship for Document in DocumentMetadata

--- src/include/database/models/entity/metadata.py
+++ src/include/database/models/entity/metadata.py
@@ -1,9 +1,10 @@
 __all__ = ["DocumentMetadata"]
 
 from sqlalchemy import VARCHAR, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from include.database.handler import Base
+from include.database.models.entity.obj import Document
 
 
 class DocumentMetadata(Base):
@@ -11,5 +12,10 @@ class DocumentMetadata(Base):
 
     document_id: Mapped[str] = mapped_column(
         VARCHAR(64), ForeignKey("document.id", ondelete="CASCADE"), primary_key=True
+    )
+    document: Mapped["Document"] = relationship(
+        "Document",
+        back_populates="metadata",
+        foreign_keys=[document_id],
     )
     # TODO: Add more metadata fields as needed, e.g. author, creation date, etc.
