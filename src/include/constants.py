79d2e411d9b8ecb981f319949e9371498527c3cd
Creeper19472 <creeper19472@crpteam.club>
2026-06-18 17:18:06 +0800

    chore(version): update CORE_VERSION to 0.3.0.260618_alpha

--- src/include/constants.py
+++ src/include/constants.py
@@ -21,7 +21,7 @@ from pathlib import Path
 
 from include.classes.version import Version
 
-CORE_VERSION = Version("0.3.0.260617_alpha")
+CORE_VERSION = Version("0.3.0.260618_alpha")
 PROTOCOL_VERSION = 14
 
 ROOT_ABSPATH = Path(__file__).resolve().parent.parent
